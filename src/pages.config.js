3e5bb10014cd617f560190dfafa272f3dc9468fa
Craig DeLong <craig.delong.mhc@gmail.com>
2025-11-27 03:07:22 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,6 +2,7 @@ import Home from './pages/Home';
 import LossBudget from './pages/LossBudget';
 import OLTSTest from './pages/OLTSTest';
 import FiberDoctor from './pages/FiberDoctor';
+import Cleaning from './pages/Cleaning';
 
 
 export const PAGES = {
@@ -9,6 +10,7 @@ export const PAGES = {
     "LossBudget": LossBudget,
     "OLTSTest": OLTSTest,
     "FiberDoctor": FiberDoctor,
+    "Cleaning": Cleaning,
 }
 
 export const pagesConfig = {
