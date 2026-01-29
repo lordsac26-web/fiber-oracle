efe6bd510ec4ade4a38f0c48cf8fb884afba1ccd
Craig DeLong <craig.delong.mhc@gmail.com>
2026-01-29 05:25:15 +0000

    Manual code change

--- functions/cleanupOldOntRecords.ts
+++ functions/cleanupOldOntRecords.ts
@@ -14,7 +14,7 @@ Deno.serve(async (req) => {
       batch_size = 100, 
       max_batches = 100,
       dry_run = false,
-      delay_between_deletes = 500,
+      delay_between_deletes = 400,
       delay_between_batches = 1500
     } = await req.json().catch(() => ({}));
 
