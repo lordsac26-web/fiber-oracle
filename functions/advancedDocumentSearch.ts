695c173b9180127c092351150b77b4f95963df04
Craig DeLong <craig.delong.mhc@gmail.com>
2026-03-07 00:52:12 +0000

    File changes

--- functions/advancedDocumentSearch.ts
+++ functions/advancedDocumentSearch.ts
@@ -1,5 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
