518a1a4538976efbe451f71ee4ae5ab5e95e163b
Craig DeLong <craig.delong.mhc@gmail.com>
2026-03-07 01:00:22 +0000

    File changes

--- functions/queryLargeDocument.ts
+++ functions/queryLargeDocument.ts
@@ -3,12 +3,6 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
-
-    if (!user) {
-      return Response.json({ error: 'Unauthorized' }, { status: 401 });
-    }
-
     const { document_id, query, max_chunks = 3 } = await req.json();
 
     if (!document_id) {
